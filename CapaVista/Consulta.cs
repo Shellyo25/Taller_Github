5e5683f50ee37636f407992922a35da75f57b40d
Shellyo25 <123606932+Shellyo25@users.noreply.github.com>
2024-09-08 19:13:08 -0600

    Revert "Botón modificar"

--- CapaVista/Consulta.cs
+++ CapaVista/Consulta.cs
@@ -22,12 +22,13 @@ namespace CapaVista
 
         public void actualizardatagridview()
         {
-            
+            DataTable dt = cn.llenarTbl(tabla);
+            Dgv_consulta.DataSource = dt;
         }
 
         private void BtnConsulta_Click(object sender, EventArgs e)
         {
-            
+            actualizardatagridview();
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -37,22 +38,64 @@ namespace CapaVista
 
         private void btn_ingresar_Click(object sender, EventArgs e)
         {
-            
+            string codigotext = txt_codigo.Text;
+            int codigo = Convert.ToInt32(codigotext);
+            string nombre = txt_nombre.Text;
+            string puesto = txt_puesto.Text;
+            string departamento = txt_departamento.Text;
+            string estadotexto = txt_estado.Text;
+            int estado = Convert.ToInt32(estadotexto);
+
+            try
+            {
+                cn.saveEmpleado(codigo, nombre, puesto, departamento, estado);
+                MessageBox.Show("Registro Agregado correctamente :)");
+            }
+            catch
+            {
+                MessageBox.Show("Registro No ingresado");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
-            
+            if (MessageBox.Show("¿Esta seguro que desea eliminar este registro?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Controlador ctriv = new Controlador();
+                if (Dgv_consulta.SelectedRows.Count > 0)
+                {
+                    // Obtener la primera fila seleccionada
+                    DataGridViewRow selectedRow = Dgv_consulta.SelectedRows[0];
+
+                    // Obtiene el valor de la primera celda de esa fila y la convierte a entero
+                    if (selectedRow.Cells[0].Value != null)
+                    {
+                        int llave = Convert.ToInt32(selectedRow.Cells[0].Value);
+                        ctriv.eliminar(llave);
+                        MessageBox.Show("Eliminado Exitosamente");
+                    }
+                }
+                else
+                {
+                    // Manejar el caso en el que no hay filas seleccionadas
+                    MessageBox.Show("No hay filas seleccionadas en el DataGridView.");
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            actualizardatagridview();
         }
 
         private void Dgv_consulta_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            txt_codigo.Text = Dgv_consulta.CurrentRow.Cells[0].Value.ToString();
+            txt_nombre.Text = Dgv_consulta.CurrentRow.Cells[1].Value.ToString();
+            txt_puesto.Text = Dgv_consulta.CurrentRow.Cells[2].Value.ToString();
+            txt_departamento.Text = Dgv_consulta.CurrentRow.Cells[3].Value.ToString();
+            txt_estado.Text = Dgv_consulta.CurrentRow.Cells[4].Value.ToString();
        
         }
 
