0e4fa0a0b0c552657a69e5ad7664611e5ec1fcba
SC <wendysacayon34@gmail.com>
2024-09-09 22:32:45 -0600

    Subo Ingreso

--- CapaVista/Consulta.cs
+++ CapaVista/Consulta.cs
@@ -1,13 +1,13 @@
-﻿//using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-//using System.Data;
+using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using CapaControlador;
+using CapaControlador;
 
 namespace CapaVista
 {
@@ -17,18 +17,18 @@ namespace CapaVista
         Controlador cn = new Controlador();
         public Consulta()
         {
-           // InitializeComponent();
+            InitializeComponent();
         }
 
         public void actualizardatagridview()
         {
-            //DataTable dt = cn.llenarTbl(tabla);
+            DataTable dt = cn.llenarTbl(tabla);
             Dgv_consulta.DataSource = dt;
         }
 
         private void BtnConsulta_Click(object sender, EventArgs e)
         {
-            //actualizardatagridview();
+            actualizardatagridview();
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -38,12 +38,29 @@ namespace CapaVista
 
         private void btn_ingresar_Click(object sender, EventArgs e)
         {
-            
+            string codigotext = txt_codigo.Text;
+            int codigo = Convert.ToInt32(codigotext);
+            string nombre = txt_nombre.Text;
+            string puesto = txt_puesto.Text;
+            string departamento = txt_departamento.Text;
+            string estadotexto = txt_estado.Text;
+            int estado = Convert.ToInt32(estadotexto);
+
+            try
+            {
+                cn.saveEmpleado(codigo, nombre, puesto, departamento, estado);
+                MessageBox.Show("Registro Agregado correctamente :)");
+            }
+            catch
+            {
+                MessageBox.Show("Registro No ingresado");
+            }
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
